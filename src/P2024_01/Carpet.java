f1464b5c99a8eb6404b54c9f0320e81b6b4032eb
ARProxy <hocci0222@kakao.com>
2023-12-30 22:46:31 +0900

    programmers carpet

--- src/P2024_01/Carpet.java
+++ src/P2024_01/Carpet.java
@@ -1,9 +1,11 @@
 package P2024_01;
 
 public class Carpet {
-    int brown = 10;
-    int yellow = 2;
+    public static void main(String[] args) {
+        int brown = 10;
+        int yellow = 2;
 
-
+        System.out.println(brown);
+    }
 }
 //어떻게 노랭이 2개 주변으로 채운 사각형 브라운 10개를 만들까
