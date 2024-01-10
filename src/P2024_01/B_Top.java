2939d5e6be0263055d01f335887f7d49b31f5ce3
ARProxy <hocci0222@kakao.com>
2024-01-10 14:24:51 +0900

    top

--- src/P2024_01/B_Top.java
+++ src/P2024_01/B_Top.java
@@ -1,5 +1,7 @@
 package P2024_01;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,7 +9,7 @@ public class B_Top {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] heights = new int[n];
+        int heights[] = new int[n];
         for(int i=0; i<n; i++) {
             heights[i] = sc.nextInt();
         }
@@ -16,20 +18,33 @@ public class B_Top {
         for(int i=0; i<n; i++) {
             while(!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
                 stack.pop();
-                System.out.println(stack);
             }
             if(stack.isEmpty()) {
                 result[i] = 0;
             } else {
                 result[i] = stack.peek() + 1;
-                System.out.println(stack);
             }
             stack.push(i);
-            System.out.println(stack);
         }
         for(int i=0; i<n; i++) {
             System.out.print(result[i] + " ");
         }
+//        List<Integer> towers = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            towers.add(sc.nextInt());
+//        }
+//
+//        List<Integer> result = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            result.add(0);
+//            for (int j = i - 1; j >= 0; j--) {
+//                if (towers.get(j) > towers.get(i)) {
+//                    result.set(i, j + 1);
+//                    System.out.println(result);
+//                    break;
+//                }
+//            }
+//        }
     }
 }
 //
