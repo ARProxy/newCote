eec9f1b70f90abe1da62ddb6c75ee7d00b8d7c07
ARProxy <hocci0222@kakao.com>
2024-02-02 02:36:11 +0900

    tet

--- src/P2024_01/dateTest.java
+++ src/P2024_01/dateTest.java
@@ -2,11 +2,13 @@ package P2024_01;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 
 public class dateTest {
     public static void main(String[] args) {
-        String date = "2024-02-02";
-        System.out.println(LocalDate.parse(date));
+        String date = "2024-02";
+        LocalDate month = LocalDate.parse(date);
+        System.out.println(month.getMonthValue());
 
     }
 }
